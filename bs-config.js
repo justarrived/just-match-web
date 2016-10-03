40e7ff639a5e2d4bc8631cc563641d1d575a1e74
Martin Belev <martinbelev93@gmail.com>
2016-10-03 16:07:30 +0300

    Make bs-config.js for development

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   port: 8000,
   files: [
-    'dist/**/*.{html,htm,css,js}'
+    './**/*.{html,htm,css,js}'
   ],
   server: {
-    baseDir: 'dist/',
+    baseDir: './',
     middleware: {
       0: null // removes default 'connect-logger' middleware
     }
