3d9d3c520fbc625593d7b4764986f79fac42449c
Martin Belev <martinbelev93@gmail.com>
2016-10-05 11:56:33 +0300

    Fix typo

--- app/config/global-exception-handler.ts
+++ app/config/global-exception-handler.ts
@@ -1,6 +1,6 @@
 import {ErrorHandler} from "@angular/core";
 
 export class GlobalExceptionHandler implements ErrorHandler {
-  handleError(error) { //TODO: remove global error meesage in console in case of exception; Think for better solution
+  handleError(error) { //TODO: remove global error message in console in case of exception; Think for better solution
   }
 }
