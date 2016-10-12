4c3ca7d23d52f363b7779197d3e1b7db3a912325
Teodor Tunev <teodortunev@gmail.com>
2016-10-12 16:36:47 +0300

    JS-3: Add slider functionality

--- app/components/slider/slider.component.ts
+++ app/components/slider/slider.component.ts
@@ -9,28 +9,55 @@ import {Job} from "../../models/job/job";
 })
 export class SliderComponent implements OnInit {
   @Input() jobs: Array<Job>;
+  private slider: any;
   private sliderContainer: any;
-  private isSliderContainerClicked: boolean = false;
+  private firstItemContainer: any;
+  private lastFullyVisualizatedItemIndex: number;
   isCompanyUser: boolean = false;
 
-  constructor(private slider: ElementRef) {
-    console.log(this.slider.nativeElement.getElementsByClassName('slider'));
+  constructor(private sliderElement: ElementRef) {
   }
 
   ngOnInit() {
-    this.sliderContainer = this.slider.nativeElement.getElementsByClassName('slider')[0];
+    this.slider = this.sliderElement.nativeElement.getElementsByClassName('slider')[0];
+    this.sliderContainer = this.sliderElement.nativeElement.getElementsByClassName('slider-container')[0];
   }
 
-  swipeLeft(event) {
-    console.log('swipeLeft');
-  }
+  moveItemContainer(isMoveToLeft: boolean) {
+    let firstItemContainerOffsetWidth = this.getFirstItemContainer().offsetWidth;
+    let maxVisualizatedItems = this.getMaxVisualisationItemContainers(firstItemContainerOffsetWidth);
+    if (!this.lastFullyVisualizatedItemIndex) {
+      this.lastFullyVisualizatedItemIndex = maxVisualizatedItems;
+    }
 
-  swipeRight() {
+    let sliderContainerLeftPosition = parseInt(this.sliderContainer.style.left || 0);
+    let pixelsValueForMovingSliderContainer = parseInt(firstItemContainerOffsetWidth * maxVisualizatedItems, 10);
+
+    if (this.lastFullyVisualizatedItemIndex + maxVisualizatedItems >= this.jobs.length && isMoveToLeft) {
+      this.sliderContainer.style.left = (((this.jobs.length * firstItemContainerOffsetWidth) - this.slider.offsetWidth) * -1) + 'px';
+      this.lastFullyVisualizatedItemIndex = this.jobs.length;
+    } else if (this.lastFullyVisualizatedItemIndex - maxVisualizatedItems <= maxVisualizatedItems && !isMoveToLeft) {
+      this.sliderContainer.style.left = 0;
+      this.lastFullyVisualizatedItemIndex = maxVisualizatedItems;
+    } else {
+      let directionMultiplier = isMoveToLeft ? -1 : 1;
+      let indexMultiplier = isMoveToLeft ? 1 : -1;
+      this.sliderContainer.style.left = (sliderContainerLeftPosition + ((pixelsValueForMovingSliderContainer - 10) * directionMultiplier)) + 'px';
+      this.lastFullyVisualizatedItemIndex  = this.lastFullyVisualizatedItemIndex + (maxVisualizatedItems * indexMultiplier);
+    }
 
   }
 
 
   resize(event) {
-    console.log('resize', event);
+    //TODO: make responsivnes on resizing
+  }
+
+  private getFirstItemContainer(): any {
+    return this.firstItemContainer = this.sliderElement.nativeElement.getElementsByClassName('item-container')[0];
+  }
+
+  private getMaxVisualisationItemContainers(itemContainerWidth): number {
+    return parseInt(this.slider.offsetWidth / itemContainerWidth, 10);
   }
 }
