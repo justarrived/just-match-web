07e09a7b6760ba1d3905cc3313a98feeb55a473e
Jacob Burenstam <burenstam@gmail.com>
2016-12-11 17:36:09 +0100

    Implement CookieStorage

--- src/app/services/data-store.service.ts
+++ src/app/services/data-store.service.ts
@@ -9,6 +9,100 @@ interface StorageInterface {
   persistsSession(): boolean;
 }
 
+class CookieStorage implements StorageInterface {
+  private data: Object = {};
+  private length: number = 0;
+  private DOM: any;
+
+  constructor(document: any) {
+    this.DOM = document;
+
+    // initialise if there's already data
+    this.data = this.getData();
+  }
+
+  public clear(): void {
+    this.data = {};
+    this.length = 0;
+    this.clearData();
+  }
+
+  public getItem(key: string): string {
+    return this.data[key] === undefined ? null : this.data[key];
+  }
+
+  public removeItem(key: string): string {
+    const oldValue = this.data[key];
+    delete this.data[key];
+
+    this.length--;
+    this.setData(this.data);
+
+    return oldValue;
+  }
+
+  public setItem(key: string, value: string): void {
+    this.data[key] = value;
+    this.length++;
+    this.setData(this.data);
+  }
+
+  public persistsRefresh(): boolean {
+    return true;
+  }
+
+  public persistsSession(): boolean {
+    return true;
+  }
+
+  private createCookie(name: string, value: string, days: number): void {
+    let expires;
+
+    if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toUTCString();
+    } else {
+      expires = '';
+    }
+
+    this.DOM.cookie = name + '=' + value + expires + '; path=/';
+  }
+
+  private readCookie(name: string) {
+    const nameEQ: string = name + '=';
+    const cookieParts: Array<string> = this.DOM.cookie.split(';');
+    let data: string = null;
+
+    cookieParts.forEach((c: string) => {
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1, c.length);
+      }
+
+      if (c.indexOf(nameEQ) == 0) {
+        data = c.substring(nameEQ.length, c.length);
+        return;
+      }
+    });
+
+    return data;
+  }
+
+  private setData(data: any) {
+    data = JSON.stringify(data);
+    this.createCookie('localStorage', data, 365);
+  }
+
+  private clearData(): void {
+    this.createCookie('localStorage', '', 365);
+  }
+
+  private getData(): Object {
+    const data = this.readCookie('localStorage');
+    return data ? JSON.parse(data) : {};
+  }
+}
+
 class MemoryStorage implements StorageInterface {
   private items: Object = {};
 
@@ -108,7 +202,8 @@ export class DataStore {
     } else if (storageTypeAvailable('sessionStorage')) {
       this.store = new SessionStorage(sessionStorage);
     } else {
-      this.store = new MemoryStorage();
+      this.store = new CookieStorage(document);
+      // this.store = new MemoryStorage();
     }
   }
 
