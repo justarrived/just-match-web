7a86902869c99f59875f7f906abb8b96df03e498
bex <danbacks@student.chalmers.se>
2016-12-16 23:57:02 +0100

    login redesign and input validation

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,18 +1,32 @@
 import {Component} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {AuthManager} from '../../services/auth-manager.service';
 import {Router} from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email: string;
-  password: string;
+  loginForm: FormGroup;
+  error_message: string;
 
-  constructor(private authManager: AuthManager, private router: Router) { }
-
-  onLoginButtonClick() {
-    this.authManager.logUser(this.email, this.password).then(result => this.router.navigate(['/home']));
+  constructor(private authManager: AuthManager, private router: Router, private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      'email': [null, Validators.compose([Validators.required])],
+      'password': [null, Validators.compose([Validators.required])]
+    })
   }
 
+  submitForm(value: any) {
+    this.authManager.logUser(value.email, value.password)
+      .then(result => this.router.navigate(['/home']))
+      .catch(errors => {
+        if (errors.details) {
+          this.error_message = errors.details.password;
+        } else {
+          console.log(errors);
+        }
+      });
+  }
 }
