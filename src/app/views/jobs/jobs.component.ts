28034550333ba04d2dc5b6acdafba45836c5fea6
bex <danbacks@student.chalmers.se>
2016-12-27 23:26:16 +0100

    map marker info window on hover

--- src/app/views/jobs/jobs.component.ts
+++ src/app/views/jobs/jobs.component.ts
@@ -22,12 +22,8 @@ export class JobsComponent extends TranslationListener implements OnInit {
 
 
   mapZoom: number = 12;
-  mapLocation: MapLocation = new MapLocation({
-    longitude: 18.0675109,
-    latitude: 59.3349086
-  });
+  mapLocation: MapLocation = new MapLocation({ longitude: 18.0675109, latitude: 59.3349086 });
   mapUserLocation: MapLocation;
-  mapError: string;
   mapStyles = [{ "elementType": "geometry", "stylers": [{ "hue": "#ff4400" }, { "saturation": -68 }, { "lightness": -4 }, { "gamma": 0.72 }] }, { "featureType": "road", "elementType": "labels.icon" }, { "featureType": "landscape.man_made", "elementType": "geometry", "stylers": [{ "hue": "#0077ff" }, { "gamma": 3.1 }] }, { "featureType": "water", "stylers": [{ "hue": "#00ccff" }, { "gamma": 0.44 }, { "saturation": -33 }] }, { "featureType": "poi.park", "stylers": [{ "hue": "#44ff00" }, { "saturation": -23 }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "hue": "#007fff" }, { "gamma": 0.77 }, { "saturation": 65 }, { "lightness": 99 }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "gamma": 0.11 }, { "weight": 5.6 }, { "saturation": 99 }, { "hue": "#0091ff" }, { "lightness": -86 }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "lightness": -48 }, { "hue": "#ff5e00" }, { "gamma": 1.2 }, { "saturation": -23 }] }, { "featureType": "transit", "elementType": "labels.text.stroke", "stylers": [{ "saturation": -64 }, { "hue": "#ff9100" }, { "lightness": 16 }, { "gamma": 0.47 }, { "weight": 2.7 }] }]
 
   constructor(private router: Router, private jobProxy: JobProxy, private location: Location, private route: ActivatedRoute, protected translationService: TranslationService, private geolocationService: Geolocation) {
@@ -58,11 +54,11 @@ export class JobsComponent extends TranslationListener implements OnInit {
         this.mapUserLocation = new MapLocation({
           longitude: position.coords.longitude,
           latitude: position.coords.latitude
-        })
+        });
         this.mapLocation = this.mapUserLocation;
       },
       error => {
-        this.mapError = error;
+        console.log(error);
       }
     );
   }
@@ -74,7 +70,6 @@ export class JobsComponent extends TranslationListener implements OnInit {
   }
 
   mapJobMarkerClicked(job) {
-    console.log("click");
     this.router.navigate(['/job', job.id]);
   }
 }
