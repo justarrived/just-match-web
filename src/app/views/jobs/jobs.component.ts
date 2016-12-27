d3927f0b2b80b475f41acfbd5729fd6108661f03
bex <danbacks@student.chalmers.se>
2016-12-28 00:46:07 +0100

    autozoom on accepting geolocation

--- src/app/views/jobs/jobs.component.ts
+++ src/app/views/jobs/jobs.component.ts
@@ -21,7 +21,7 @@ export class JobsComponent extends TranslationListener implements OnInit {
   pageSize: number = 10;
 
 
-  mapZoom: number = 12;
+  mapZoom: number = 5;
   mapLocation: MapLocation = new MapLocation({ longitude: 18.0675109, latitude: 59.3349086 });
   mapUserLocation: MapLocation;
   mapStyles = [{ "elementType": "geometry", "stylers": [{ "hue": "#ff4400" }, { "saturation": -68 }, { "lightness": -4 }, { "gamma": 0.72 }] }, { "featureType": "road", "elementType": "labels.icon" }, { "featureType": "landscape.man_made", "elementType": "geometry", "stylers": [{ "hue": "#0077ff" }, { "gamma": 3.1 }] }, { "featureType": "water", "stylers": [{ "hue": "#00ccff" }, { "gamma": 0.44 }, { "saturation": -33 }] }, { "featureType": "poi.park", "stylers": [{ "hue": "#44ff00" }, { "saturation": -23 }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "hue": "#007fff" }, { "gamma": 0.77 }, { "saturation": 65 }, { "lightness": 99 }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "gamma": 0.11 }, { "weight": 5.6 }, { "saturation": 99 }, { "hue": "#0091ff" }, { "lightness": -86 }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "lightness": -48 }, { "hue": "#ff5e00" }, { "gamma": 1.2 }, { "saturation": -23 }] }, { "featureType": "transit", "elementType": "labels.text.stroke", "stylers": [{ "saturation": -64 }, { "hue": "#ff9100" }, { "lightness": 16 }, { "gamma": 0.47 }, { "weight": 2.7 }] }]
@@ -56,6 +56,7 @@ export class JobsComponent extends TranslationListener implements OnInit {
           latitude: position.coords.latitude
         });
         this.mapLocation = this.mapUserLocation;
+        this.mapZoom = 12;
       },
       error => {
         console.log(error);
