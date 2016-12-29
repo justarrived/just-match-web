8609027dfaab414afb219981ec0f2a3bdec75fa4
bex <danbacks@student.chalmers.se>
2016-12-29 22:34:27 +0100

    Map style extracted to own constant

--- src/app/views/jobs/jobs.component.ts
+++ src/app/views/jobs/jobs.component.ts
@@ -8,6 +8,7 @@ import {Location} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {TranslationService} from '../../services/translation.service';
 import {TranslationListener} from '../../components/translation.component';
+import {customMapStyle} from '../../styles/google-maps-styles'
 
 @Component({
   templateUrl: './jobs.component.html',
@@ -20,11 +21,10 @@ export class JobsComponent extends TranslationListener implements OnInit {
   page: number = 1;
   pageSize: number = 10;
 
-
   mapZoom: number = 5;
   mapLocation: MapLocation = new MapLocation({ longitude: 18.0675109, latitude: 59.3349086 });
   mapUserLocation: MapLocation;
-  mapStyles = [{ "elementType": "geometry", "stylers": [{ "hue": "#ff4400" }, { "saturation": -68 }, { "lightness": -4 }, { "gamma": 0.72 }] }, { "featureType": "road", "elementType": "labels.icon" }, { "featureType": "landscape.man_made", "elementType": "geometry", "stylers": [{ "hue": "#0077ff" }, { "gamma": 3.1 }] }, { "featureType": "water", "stylers": [{ "hue": "#00ccff" }, { "gamma": 0.44 }, { "saturation": -33 }] }, { "featureType": "poi.park", "stylers": [{ "hue": "#44ff00" }, { "saturation": -23 }] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "hue": "#007fff" }, { "gamma": 0.77 }, { "saturation": 65 }, { "lightness": 99 }] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [{ "gamma": 0.11 }, { "weight": 5.6 }, { "saturation": 99 }, { "hue": "#0091ff" }, { "lightness": -86 }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "lightness": -48 }, { "hue": "#ff5e00" }, { "gamma": 1.2 }, { "saturation": -23 }] }, { "featureType": "transit", "elementType": "labels.text.stroke", "stylers": [{ "saturation": -64 }, { "hue": "#ff9100" }, { "lightness": 16 }, { "gamma": 0.47 }, { "weight": 2.7 }] }]
+  mapStyles = customMapStyle;
 
   constructor(private router: Router, private jobProxy: JobProxy, private location: Location, private route: ActivatedRoute, protected translationService: TranslationService, private geolocationService: Geolocation) {
     super(translationService);
