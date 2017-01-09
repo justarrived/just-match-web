9de357e8ecae0ce59eb569772d24805e3d567669
bex <danbacks@student.chalmers.se>
2017-01-10 00:37:05 +0100

    fix missing attribute complaint

--- src/app/utils/image-to-data-url.util.ts
+++ src/app/utils/image-to-data-url.util.ts
@@ -2,7 +2,7 @@ export function getDataUrl(file) {
   return new Promise((resolve, reject) => {
     var reader = new FileReader();
     reader.onload = function(event) {
-      resolve(event.target.result);
+      resolve(event.target['result']);
     };
     reader.readAsDataURL(file);
   });
