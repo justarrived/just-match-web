d9e561f8ab97b1d1993add23d6e95689643f6e48
bex <danbacks@student.chalmers.se>
2017-01-18 18:31:15 +0100

    Loading gif component added

---
+++ src/app/components/loading-gif/loading-gif.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'loading-gif',
+  templateUrl: './loading-gif.component.html',
+  styleUrls: ['./loading-gif.component.scss']
+})
+export class LoadingGifComponent {
+  constructor() {
+  }
+}
