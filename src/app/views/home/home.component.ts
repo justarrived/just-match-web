d28acf765a0ac04b361f67a45cbb2535e668f5e2
Philip Ekman <ekman1991@gmail.com>
2017-01-18 18:11:30 +0100

    Load UserJobs from API

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,25 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {JobProxy} from '../../services/proxy/job-proxy.service';
+import {UserProxy} from '../../services/proxy/user-proxy.service';
 import {Job} from '../../models/job/job';
+import {UserJob} from '../../models/user/user-job';
 import {TranslationService} from '../../services/translation.service';
 import {UserManager} from '../../services/user-manager.service';
 import {User} from '../../models/user';
 import {TranslationListener} from '../../components/translation.component';
+import {isEmpty} from 'lodash';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers: [JobProxy]
+  providers: [JobProxy, UserProxy]
 })
 export class HomeComponent extends TranslationListener implements OnInit {
   today: number = new Date().getDate();
   email: string;
   password: string;
   newJobs: Job[];
+  userJobs: UserJob[];
   isCompanyUser: boolean;
   user: User;
+  isEmpty = isEmpty;
 
-  constructor(private jobProxy: JobProxy, private userManager: UserManager, protected translationService: TranslationService) {
+  constructor(private jobProxy: JobProxy, private userProxy: UserProxy, private userManager: UserManager, protected translationService: TranslationService) {
     super(translationService);
 
     this.isCompanyUser = userManager.isCompanyUser();
@@ -28,6 +33,7 @@ export class HomeComponent extends TranslationListener implements OnInit {
 
   ngOnInit() {
     this.loadData();
+    this.loadUserJobs();
   }
 
   loadData(): void {
@@ -36,4 +42,19 @@ export class HomeComponent extends TranslationListener implements OnInit {
         this.newJobs = result.data;
       });
   }
+
+  loadUserJobs(): void {
+    this.userProxy.getUserJobs(this.user.id, {include: 'job'})
+      .then(result => {
+        this.userJobs = result;
+      });
+  }
+
+  userJobsVisible() {
+    if(this.user) {
+      return !this.isEmpty(this.userJobs);
+    }
+    return false;
+  }
+
 }
