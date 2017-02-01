2146398d00e1358a8f5fed29ce05eecb15935185
bex <danbacks@student.chalmers.se>
2017-02-01 22:32:32 +0100

    Route url should start with / to route from root of the app

--- src/app/routes/ja-routes.ts
+++ src/app/routes/ja-routes.ts
@@ -2,17 +2,17 @@ import {JARoute} from './ja-route';
 
 export class JARoutes {
   public static home: JARoute = { url: () => ''};
-  public static login: JARoute = { url: () => 'login'};
-  public static forgotPassword: JARoute = { url: () => 'reset-password'};
-  public static faq: JARoute = { url: () => 'faq'};
-  public static contact: JARoute = { url: () => 'contact'};
-  public static confirmation: JARoute = { url: (args?: string[]) => 'confirmation/' + args[0]};
-  public static aboutCookies: JARoute = { url: () => 'cookies-about'};
-  public static jobs: JARoute = { url: (args?: string[]) => 'jobs/' + args[0]};
-  public static job: JARoute = { url: (args?: string[]) => 'job/' + args[0]};
-  public static registerUser: JARoute = { url: () => 'user/register'};
-  public static user: JARoute = { url: (args?: string[]) => 'users/' + args[0]};
-  public static userJobs: JARoute = { url: (args?: string[]) => 'users/' + args[0] + '/jobs'};
-  public static error: JARoute = { url: (args?: string[]) => 'error/' + args[0]};
-  public static notFound: JARoute = { url: () => '404'};
+  public static login: JARoute = { url: () => '/login'};
+  public static forgotPassword: JARoute = { url: () => '/reset-password'};
+  public static faq: JARoute = { url: () => '/faq'};
+  public static contact: JARoute = { url: () => '/contact'};
+  public static confirmation: JARoute = { url: (args?: string[]) => '/confirmation/' + args[0]};
+  public static aboutCookies: JARoute = { url: () => '/cookies-about'};
+  public static jobs: JARoute = { url: (args?: string[]) => '/jobs/' + args[0]};
+  public static job: JARoute = { url: (args?: string[]) => '/job/' + args[0]};
+  public static registerUser: JARoute = { url: () => '/user/register'};
+  public static user: JARoute = { url: (args?: string[]) => '/users/' + args[0]};
+  public static userJobs: JARoute = { url: (args?: string[]) => '/users/' + args[0] + '/jobs'};
+  public static error: JARoute = { url: (args?: string[]) => '/error/' + args[0]};
+  public static notFound: JARoute = { url: () => '/404'};
 }
