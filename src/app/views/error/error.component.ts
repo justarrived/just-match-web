1ecb9172ba9f7f2387fad391603bee3773fdd780
bex <danbacks@student.chalmers.se>
2017-02-14 23:57:36 +0100

    error component refactoring

--- src/app/views/error/error.component.ts
+++ src/app/views/error/error.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: './error.component.html',
-    styleUrls: ['./error.component.scss'],
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent {
 
