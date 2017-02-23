8116aa39fcd00136773a9a03a9c169aaa7524e10
Philip Ekman <ekman1991@gmail.com>
2017-02-23 12:03:55 +0100

    Added god-mode component

---
+++ src/app/views/god-mode/god-mode.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './god-mode.component.html',
+  styleUrls: ['./god-mode.component.scss'],
+})
+export class GodModeComponent {
+
+  constructor(
+  ) {
+  }
+}
