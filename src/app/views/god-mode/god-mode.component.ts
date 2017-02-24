cdf57484205da87251b8681b1b24ed4b07ebf07a
Philip Ekman <ekman1991@gmail.com>
2017-02-24 13:34:06 +0100

    Template code for entering god-mode

--- src/app/views/god-mode/god-mode.component.ts
+++ src/app/views/god-mode/god-mode.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, ElementRef, Input} from '@angular/core';
+import {UserProxy} from '../../services/proxy/user-proxy.service';
+import {User} from '../../models/user';
+import {ActsAsUser} from '../../services/acts-as-user.service';
 
 @Component({
   templateUrl: './god-mode.component.html',
@@ -6,7 +9,31 @@ import {Component} from '@angular/core';
 })
 export class GodModeComponent {
 
+  private users: Array<User>;
+
   constructor(
+    private userProxy: UserProxy,
+    private actsAsUser: ActsAsUser
   ) {
   }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.loadUsers();
+  }
+
+  private loadUsers(): void {
+    this.userProxy.getUsers({'page[size]': 50, 'fields[user]' : 'first_name, last_name'})
+      .then(result => {
+        this.users = result.data;
+      });
+  }
+
+  private activateGodMode(): void {
+    this.actsAsUser.setUserId("5");
+  }
+
 }
