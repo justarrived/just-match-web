8cd92c86c7c94884e4414ceeb98f26e7c69cede3
Jacob Burenstam <burenstam@gmail.com>
2017-02-28 21:19:12 +0100

    Refactor user-register.component to user input-errors.component

--- src/app/components/form-parts/input-error.component.ts
+++ src/app/components/form-parts/input-error.component.ts
@@ -2,17 +2,17 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'input-error',
-  template: `<div *ngIf="showErrorLabel" class="ui pointing red basic label">{{label}}</div>`
+  template: `<div *ngIf="visible" class="ui pointing red basic label">{{label}}</div>`
 })
 export class InputErrorComponent {
   @Input() label: string;
-  showErrorLabel: boolean = true;
+  @Input() visible: boolean = true;
 
   hideError(): void {
-    this.showErrorLabel = false;
+    this.visible = false;
   }
 
   showError(): void {
-    this.showErrorLabel = true;
+    this.visible = true;
   }
 }
