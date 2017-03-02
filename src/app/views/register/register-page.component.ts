eba0b8093ebe86a9c2851756b7e8eb3988f8107f
bex <danbacks@student.chalmers.se>
2017-03-02 18:17:07 +0100

    inline register page html

--- src/app/views/register/register-page.component.ts
+++ src/app/views/register/register-page.component.ts
@@ -2,6 +2,21 @@ import {Component} from '@angular/core';
 
 @Component({
   styleUrls: ['./register-page.component.scss'],
-  templateUrl: './register-page.component.html'
+  template: `
+  <div class="register-container">
+
+    <div class="sub-header-container">
+      <h3 class="sub-header-title">
+        {{"user.register.title" | translate}}
+      </h3>
+    </div>
+
+    <div class="ui centered grid">
+      <div class="fourteen wide phone ten wide tablet eight wide computer column">
+
+        <register-form></register-form>
+      </div>
+    </div>
+  </div>`
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent { }
