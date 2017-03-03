c151db19c6e816443a57e247764211414eee621b
Philip Ekman <ekman1991@gmail.com>
2017-03-03 15:07:45 +0100

    Implemented first-name component

---
+++ src/app/components/form-parts/inputs/user-first-name-input/user-first-name-input.component.ts
@@ -0,0 +1,23 @@
+import {ApiErrors} from '../../../../models/api-errors';
+import {Component} from '@angular/core';
+import {Input} from '@angular/core';
+
+
+@Component({
+  selector: 'user-first-name-input',
+  template: `
+  <text-input
+    [apiErrors]="apiErrors"
+    [control]="control"
+    [label]="('form.parts.user.first.name.input.label' | translate) + ' *'"
+    [placeholder]="'form.parts.user.first.name.input.placeholder' | translate"
+    apiAttribute="first_name"
+    icon="user"
+    type="text">
+  </text-input>
+  `
+})
+export class UserFirstNameInputComponent {
+  @Input() apiErrors: any;
+  @Input() control: any;
+}
