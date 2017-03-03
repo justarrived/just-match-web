69c196e74aab3edea4afa6630ca69115c8c9f37d
Jacob Burenstam <burenstam@gmail.com>
2017-03-03 09:52:54 +0100

    Add gzip compression to node server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // server.js
 const path = require('path');
+const compression = require('compression');
 const express = require('express');
 const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// Use Gzip compression
+app.use(compression());
 
 // Start the app by listening on the default
 // Heroku port
