fa62a3dde64364a46a5bef5a18cf319b67169c06
Philip Ekman <ekman1991@gmail.com>
2017-03-03 15:27:46 +0100

    Added validators and field div

--- src/app/components/form-parts/inputs/text-input/text-input.component.ts
+++ src/app/components/form-parts/inputs/text-input/text-input.component.ts
@@ -5,27 +5,37 @@ import {Input} from '@angular/core';
 @Component({
   selector: 'text-input',
   template: `
-  <sm-input
-    [control]="control"
-    [label]="label"
-    [placeholder]="placeholder"
-    class="left"
-    [icon]="icon"
-    [type]="type">
-  </sm-input>
-  <input-errors
-    [apiErrors]="apiErrors"
-    [control]="control"
-    [apiAttribute]="apiAttribute">
-  </input-errors>
+  <div class="field">
+    <sm-input
+      [control]="control"
+      [label]="label"
+      [placeholder]="placeholder"
+      class="left"
+      [icon]="icon"
+      [type]="type">
+    </sm-input>
+    <input-errors
+      [apiErrors]="apiErrors"
+      [control]="control"
+      [apiAttribute]="apiAttribute"
+      [patternLabel]="patternLabel"
+      [requiredLabel]="requiredLabel"
+      [minLengthLabel]="minLengthLabel"
+      [maxLengthLabel]="maxLengthLabel">
+    </input-errors>
+  </div>
   `
 })
 export class TextInputComponent {
-  @Input() apiErrors: any;
-  @Input() control: any;
-  @Input() label: any;
-  @Input() placeholder: any;
-  @Input() icon: string;
-  @Input() type: string;
-  @Input() apiAttribute: string;
+  @Input() public apiErrors: any;
+  @Input() public control: any;
+  @Input() public label: any;
+  @Input() public placeholder: any;
+  @Input() public icon: string;
+  @Input() public type: string;
+  @Input() public apiAttribute: string;
+  @Input() public patternLabel: string;
+  @Input() public requiredLabel: string;
+  @Input() public minLengthLabel: string;
+  @Input() public maxLengthLabel: string;
 }
