b45ba747eeb5246689128abd53ec18119668d4c2
bex <danbacks@student.chalmers.se>
2017-03-04 13:09:25 +0100

    404 refactoring

--- src/app/views/404/404.component.ts
+++ src/app/views/404/404.component.ts
@@ -1,7 +1,16 @@
 import {Component} from '@angular/core';
 
 @Component({
-  templateUrl: './404.component.html',
-  styleUrls: ['./404.component.scss'],
+  template: `
+  <basic-border-header
+    [header]="'page.not.found.error.title' | translate"
+    icon="exclamation triangle">
+  </basic-border-header>
+  <div class="ui basic very padded segment">
+    <error-message
+      header="{{'page.not.found.error.description' | translate}} 😞"
+      icon="warning">
+    </error-message>
+  </div>`
 })
 export class NotFoundComponent {}
