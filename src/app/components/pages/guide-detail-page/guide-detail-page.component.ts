68bda43aaf80847b55d235b77d585e4ef21ff6cb
Philip Ekman <ekman1991@gmail.com>
2017-04-20 18:27:49 +0200

    Guide details design finished

---
+++ src/app/components/pages/guide-detail-page/guide-detail-page.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './guide-detail-page.component.html',
+  styleUrls: ['./guide-detail-page.component.scss']
+})
+export class GuideDetailPageComponent {
+
+  public items = [1,2,3,4,5]
+
+  public lol() {
+    
+  }
+
+}
