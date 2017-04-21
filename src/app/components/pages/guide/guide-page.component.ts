262506b6d19721a5ad0682a09edf355d7a7cc08e
Philip Ekman <ekman1991@gmail.com>
2017-04-21 10:30:19 +0200

    Routing to guide-detalis view

--- src/app/components/pages/guide/guide-page.component.ts
+++ src/app/components/pages/guide/guide-page.component.ts
@@ -1,15 +1,13 @@
 import {Component} from '@angular/core';
+import {JARoutes} from '../../../routes/ja-routes/ja-routes';
 
 @Component({
   templateUrl: './guide-page.component.html',
   styleUrls: ['./guide-page.component.scss']
 })
 export class GuidePageComponent {
+  public JARoutes = JARoutes;
 
-  public items = [1,2,3,4,5]
-
-  public lol() {
-    console.log("ASD");
-  }
+  public items = [1,2,3,4,5];
 
 }
