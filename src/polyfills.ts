1b2253b06b5eeac151e4948d4509cbab9d4f121f
bex <danbacks@student.chalmers.se>
2017-05-01 23:51:24 +0200

    Try to fix the sentry intl error "No locale data has been provided for this object yet"

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -62,3 +62,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 import 'intl';  // Run `npm install --save intl`.
+import 'intl/locale-data/jsonp/en.js';
+import 'intl/locale-data/jsonp/sv.js';
+import 'intl/locale-data/jsonp/ar.js';
