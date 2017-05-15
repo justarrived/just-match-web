59fcff7e8620d652e7e1a92e78a1285cc91a1626
Jacob Burenstam <burenstam@gmail.com>
2017-05-15 23:44:51 +0200

    Add p2p-env

---
+++ src/environments/environment.p2p.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  sentryURL: '',
+  googleMapsKey: 'AIzaSyABLDG-8LXDMSp6vsTNNI2uSPCWA4eNs-Y',
+  apiBaseUrl: 'https://api-p2p.justarrived.xyz/api/v1/'
+};
