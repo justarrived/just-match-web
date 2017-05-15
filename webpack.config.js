b3130adbb3191290b2ae71b41a2362ea020e95b4
bex <danbacks@student.chalmers.se>
2017-05-15 11:48:13 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
       'process.env.ENV_CONFIG': JSON.stringify(process.env.ENV_CONFIG || 'dev')
     }),
   ]
