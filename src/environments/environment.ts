0079f2d93597907899d2ea2eb9df16d1738dfdd3
bex <danbacks@student.chalmers.se>
2017-05-15 10:33:30 +0200

    server environment solution

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,8 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=production` then `environment.production.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
+import {environment as prodEnv} from './environment.prod';
+import {environment as stagingEnv} from './environment.staging';
+import {environment as devEnv} from './environment.dev';
 
-export const environment = {
-  production: false,
-  sentryURL: '',
-  googleMapsKey: 'AIzaSyABLDG-8LXDMSp6vsTNNI2uSPCWA4eNs-Y',
-  apiBaseUrl: 'https://sandbox-api.justarrived.xyz/api/v1/'
-};
+export const environment = process.env.ENV_CONFIG === 'prod' ? prodEnv : process.env.ENV_CONFIG === 'staging' ? stagingEnv : devEnv;
