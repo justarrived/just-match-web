11bd95aa2923c0e17fc32f6091e98cfad2db4dfd
Bex <bex@justarrived.se>
2017-08-14 13:35:02 +0200

    Show resume first in update profile steps

--- src/app/components/modals/update-profile-step-1-modal/update-profile-step-1-modal.component.ts
+++ src/app/components/modals/update-profile-step-1-modal/update-profile-step-1-modal.component.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 import {Input} from '@angular/core';
 import {SystemLanguagesResolver} from '../../../resolvers/system-languages/system-languages.resolver';
 import {UserResolver} from '../../../resolvers/user/user.resolver';
-import {UserUpdateFormComponent} from '../../forms/user-update-form/user-update-form.component';
 import {ViewChild} from '@angular/core';
 
 @Component({
@@ -11,8 +10,6 @@ import {ViewChild} from '@angular/core';
   template: `
     <stepper-modal
       #modal
-      [goToNextOnClick]="false"
-      (onNextClick)="nextClicked()"
       nextModal="updateProfileStep2ModalComponent">
       <modal-header>
         <basic-title-text
@@ -30,17 +27,15 @@ import {ViewChild} from '@angular/core';
           [currentStep]="1"
           [steps]="[1,2,3]">
         </basic-stepper>
-        <user-update-form
-          #userUpdateForm
-          [isInModal]="true"
-          [missingUserTraits]="{skill_ids: {}}">
-        </user-update-form>
+        <resume-input
+          [centered]="true"
+          [showLabel]="false">
+        </resume-input>
       </modal-content>
     </stepper-modal>`
 })
 export class UpdateProfileStep1ModalComponent extends BaseComponent {
   @ViewChild('modal') public modal: any;
-  @ViewChild('userUpdateForm') public userUpdateForm: UserUpdateFormComponent;
 
   public constructor(
     protected systemLanguagesResolver: SystemLanguagesResolver,
@@ -55,15 +50,5 @@ export class UpdateProfileStep1ModalComponent extends BaseComponent {
 
   public hide() {
     this.modal.hide();
-  }
-
-  public nextClicked(): void {
-    this.userUpdateForm.submitForm()
-    .then(() => {
-      this.modal.next(false);
-    })
-    .catch(() => {
-      // Handling done in form.
-    });
   }
 }
--- src/app/components/modals/update-profile-step-2-modal/update-profile-step-2-modal.component.ts
+++ src/app/components/modals/update-profile-step-2-modal/update-profile-step-2-modal.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 import {Input} from '@angular/core';
 import {SystemLanguagesResolver} from '../../../resolvers/system-languages/system-languages.resolver';
 import {UserResolver} from '../../../resolvers/user/user.resolver';
+import {UserUpdateFormComponent} from '../../forms/user-update-form/user-update-form.component';
 import {ViewChild} from '@angular/core';
 
 @Component({
@@ -10,6 +11,8 @@ import {ViewChild} from '@angular/core';
   template: `
     <stepper-modal
       #modal
+      [goToNextOnClick]="false"
+      (onNextClick)="nextClicked()"
       previousModal="updateProfileStep1ModalComponent"
       nextModal="updateProfileStep3ModalComponent">
       <modal-header>
@@ -28,15 +31,17 @@ import {ViewChild} from '@angular/core';
           [currentStep]="2"
           [steps]="[1,2,3]">
         </basic-stepper>
-        <resume-input
-          [centered]="true"
-          [showLabel]="false">
-        </resume-input>
+        <user-update-form
+          #userUpdateForm
+          [isInModal]="true"
+          [missingUserTraits]="{skill_ids: {}}">
+        </user-update-form>
       </modal-content>
     </stepper-modal>`
 })
 export class UpdateProfileStep2ModalComponent extends BaseComponent {
   @ViewChild('modal') public modal: any;
+  @ViewChild('userUpdateForm') public userUpdateForm: UserUpdateFormComponent;
 
   public constructor(
     protected systemLanguagesResolver: SystemLanguagesResolver,
@@ -51,5 +56,15 @@ export class UpdateProfileStep2ModalComponent extends BaseComponent {
 
   public hide() {
     this.modal.hide();
+  }
+
+  public nextClicked(): void {
+    this.userUpdateForm.submitForm()
+    .then(() => {
+      this.modal.next(false);
+    })
+    .catch(() => {
+      // Handling done in form.
+    });
   }
 }
