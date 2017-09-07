06f4e11ce1d7376e69cd798cefb9b66cf8031ceb
Bex <bex@justarrived.se>
2017-09-07 10:25:02 +0200

    Updated job digests model fields

--- src/app/models/api-models/job-digest/job-digest.ts
+++ src/app/models/api-models/job-digest/job-digest.ts
@@ -1,10 +1,26 @@
-import {Job} from '../job/job';
-import {JobFactory} from '../job/job';
+import {Address} from '../address/address';
+import {AddressFactory} from '../address/address';
+import {DigestSubscriber} from '../digest-subscriber/digest-subscriber';
+import {DigestSubscriberFactory} from '../digest-subscriber/digest-subscriber';
+import {map} from 'lodash';
+import {Occupation} from '../occupation/occupation';
+import {OccupationFactory} from '../occupation/occupation';
+import {User} from '../user/user';
+import {UserFactory} from '../user/user';
 
 // API attribute interfaces
 interface JobDigestApiAttributes {
+  address: Address;
+  createdAt: Date;
+  deletedAt: Date;
   id: string;
-  job: Job;
+  locale: string;
+  maxDistance: number;
+  notificationFrequency: number;
+  occupations: Occupation[];
+  subscriber: DigestSubscriber;
+  updatedAt: Date;
+  user: User;
 }
 
 // Client interfaces
@@ -19,8 +35,17 @@ export class JobDigestFactory {
     }
 
     return {
+      address: AddressFactory.createAddress(jsonObject.address),
+      createdAt: new Date(jsonObject.created_at),
+      deletedAt: new Date(jsonObject.deleted_at),
       id: jsonObject.id,
-      job: JobFactory.createJob(jsonObject.job),
+      locale: jsonObject.locale,
+      maxDistance: jsonObject.max_distance,
+      notificationFrequency: jsonObject.notification_frequency,
+      occupations: map(jsonObject.occupations, occupation => OccupationFactory.createOccupation(occupation)),
+      subscriber: DigestSubscriberFactory.createDigestSubscriber(jsonObject.subscriber),
+      updatedAt: new Date(jsonObject.updated_at),
+      user: UserFactory.createUser(jsonObject.user),
     };
   }
 }
