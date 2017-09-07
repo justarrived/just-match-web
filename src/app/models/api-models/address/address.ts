d343bb8a4339cecb99a318ec7b5f3cd66120b3be
Bex <bex@justarrived.se>
2017-09-07 10:31:06 +0200

    Added the address model

---
+++ src/app/models/api-models/address/address.ts
@@ -0,0 +1,45 @@
+// API attribute interfaces
+interface AddressApiAttributes {
+  city: string;
+  countryCode: string;
+  createdAt: Date;
+  id: string;
+  latitude: number;
+  longitude: number;
+  municipality: string;
+  postalCode: string;
+  state: string;
+  street1: string;
+  street2: string;
+  updatedAt: Date;
+  uuid: string;
+}
+
+// Client interfaces
+export interface Address extends AddressApiAttributes {
+}
+
+// Factories
+export class AddressFactory {
+  public static createAddress(jsonObject?: any): Address {
+    if (!jsonObject) {
+      return;
+    }
+
+    return {
+      city: jsonObject.city,
+      countryCode: jsonObject.country_code,
+      createdAt: new Date(jsonObject.created_at),
+      id: jsonObject.id,
+      latitude: jsonObject.latitude,
+      longitude: jsonObject.longitude,
+      municipality: jsonObject.municipality,
+      postalCode: jsonObject.postal_code,
+      state: jsonObject.state,
+      street1: jsonObject.street1,
+      street2: jsonObject.street2,
+      updatedAt: new Date(jsonObject.updated_at),
+      uuid: jsonObject.uuid,
+    };
+  }
+}
