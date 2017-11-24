36b00cadc229ca68871e1203e9eb73f3402d6745
Bex <bex@justarrived.se>
2017-11-24 14:43:53 +0100

    Add language parameter to all events

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,15 +1,19 @@
 import {Angulartics2} from 'angulartics2';
 import {Injectable} from '@angular/core';
+import {SystemLanguagesResolver} from '../resolvers/system-languages/system-languages.resolver';
 
 @Injectable()
 export class AnalyticsService {
 
   public constructor(
-    private angulartics2: Angulartics2
+    private angulartics2: Angulartics2,
+    private systemLanguagesResolver: SystemLanguagesResolver,
   ) {
   }
 
-  public publishEvent(action: string, properties?: any, ) {
+  public publishEvent(action: string, properties: any = {}, ) {
+    properties.language = this.systemLanguagesResolver.getSelectedSystemLanguage().languageCode;
+
     this.angulartics2.eventTrack.next({
       action: action,
       properties: properties
