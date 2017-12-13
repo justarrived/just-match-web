7f7460b07bf546e6f4f0ab2eb570b5a3fe0ba1ed
Jacob Burenstam <burenstam@gmail.com>
2017-12-13 14:39:56 +0100

    Re-add requirements-section to home page

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -24,7 +24,7 @@ import {UserResolver} from '../../../resolvers/user/user.resolver';
 
     <how-it-works-section *ngIf="!user"></how-it-works-section>
 
-
+    <requirements-section></requirements-section>
 
     <partners-section></partners-section>
   `
